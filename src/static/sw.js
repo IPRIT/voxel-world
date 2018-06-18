33ef73b7d6a72af33b759ffe9a9d59ccc18221b1
IPRIT <ipritoflex@yandex.ru>
2018-06-19 01:58:31 +0300

    Bumped version to 0.0.2

--- src/static/sw.js
+++ src/static/sw.js
@@ -8,11 +8,11 @@ const workboxSW = new self.WorkboxSW({
 
 workboxSW.precache([
   {
-    "url": "/_nuxt/app.934a7d453435cbb0292c.js",
-    "revision": "ef061f5d18c3c67affe8c4a6436ac7a8"
+    "url": "/_nuxt/app.67bd89072169b5a92388.js",
+    "revision": "34448c6a2d179f5f32f56fc69f42e85e"
   },
   {
-    "url": "/_nuxt/d1977b072036d54b261f.worker.js",
+    "url": "/_nuxt/db1aa2162427b65e20e7.worker.js",
     "revision": "282ec8d7ac649d5b928e985464482f51"
   },
   {
@@ -20,16 +20,16 @@ workboxSW.precache([
     "revision": "23db9bfee507678d6c170861b0ce769a"
   },
   {
-    "url": "/_nuxt/layouts_default.b3ff4383d7b47d48b960.js",
-    "revision": "d329e0b57c3a5f49b8f16e16811f1b3e"
+    "url": "/_nuxt/layouts_default.1dbf5ba97e30be97e894.js",
+    "revision": "a34445b969ad80420f0b6c092b559bf0"
   },
   {
-    "url": "/_nuxt/manifest.b58ad747c55752f00aae.js",
-    "revision": "8999fbfee4d7199c382dc9acadf866ee"
+    "url": "/_nuxt/manifest.0f4f91db70b00c17bf5e.js",
+    "revision": "4569eda74b947974e7c8c423c2051c67"
   },
   {
-    "url": "/_nuxt/pages/game-page.0d4804e15f110f0aed9e.js",
-    "revision": "327b7cf65e0fe6e844400f43c63fd064"
+    "url": "/_nuxt/pages/game-page.62bf7103bec0282b7dd9.js",
+    "revision": "857a92462d0073ab6eedacb40d1e77aa"
   },
   {
     "url": "/_nuxt/vendor.9e43d0fd2898bdff7751.js",
